eedbf50dc81108c249efe025ce1104ba8f09549d
Maxi Mendez <mmendez1720@gmail.com>
2022-03-08 22:53:29 -0300

    nuevo commit

--- components/form.js
+++ components/form.js
@@ -2,7 +2,7 @@ function formEl(el){
     const formComponent  = document.createElement("div");
     formComponent.querySelector(".form__contenedor")
     formComponent.innerHTML =
-`<div class="form_container">
+`<form class="form_container">
 <h4 class="form__name">Nombre</h4>
 <input id="name"class="form__input" type="text">
 <label class="form__lbl" for="nombre"></label>
@@ -12,20 +12,41 @@ function formEl(el){
 <h4 class="form__name">Mensaje</h4>
 <textarea id="message"class="form__textArea"></textarea>
 <button class="btn-send">Enviar</button>
-</div>`;
+</form>`;
 el.appendChild(formComponent);
+
 };
 
 function formEvent(){
-    const formComponent = document.querySelector(".form__contenedor");
-    formComponent.addEventListener("submit", function(event) {
+    const newComponenteForm = document.querySelector(".form__contenedor");
+    newComponenteForm.addEventListener("submit", function(event) {
        event.preventDefault();
-        
-       const data = {
-        to: "mmendez1720@gmail.com",
-        message: `${Nombre} quiere comuncarse con nosotros, dejo el siguiente mensaje: ${Mensaje}`};
- fetch("https://apx-api.vercel.app/api/utils/dwf", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(data),
-  }).then((res) => res.json()).catch((error) => console.error("Ocurrio un error!", error)).then(() => { console.log("salió todo ok")});});}; 
+    
+       const data = new FormData(event.target);
+       const object = Object.fromEntries(data.entries());
+         
+      const mensaje = `
+        user: ${object.nombre}
+        email: ${object.email}
+        mensaje: ${object.message}
+      `;
+    
+      fetch("https://apx-api.vercel.app/api/utils/dwf", {
+          method: "POST",
+    
+          headers: {"content-type": "application/json"},
+          body: JSON.stringify({
+              to: "mmendez1720@gmail.com",
+              message: `${object.nombre} quiere comuncarse con nosotros, dejo el siguiente mensaje: ${object.message}`,
+          })
+      })
+    .then(()=> {
+        console.log("mensaje")
+        alert("Mensaje enviado");
+    })
+    .catch(() => {
+      alert("Ha ocurrido un error");
+    });
+    });
+    }
+    formEvent()
